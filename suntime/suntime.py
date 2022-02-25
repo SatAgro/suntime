05371a7826ca0cfaba8076b09f4996cf20c73193
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-02-25 09:02:30 +0700

    reroll suntime to old functions structure

--- suntime/suntime.py
+++ suntime/suntime.py
@@ -31,7 +31,7 @@ class Sun:
         :raises: SunTimeException when there is no sunrise and sunset on given location and date
         """
         date = datetime.datetime.now() if date is None else date
-        sr = self._calc_sun_time(date, tz=tz, f1=_f1_sunrise, f2=_f2_sunrise)
+        sr = self._calc_sun_time(date, tz=tz, isRiseTime=True)
         if sr is None:
             raise SunTimeException('The sun never rises on this location (on the specified date)')
         else:
@@ -47,13 +47,13 @@ class Sun:
         :raises: SunTimeException when there is no sunrise and sunset on given location and date.
         """
         date = datetime.datetime.now() if date is None else date
-        ss = self._calc_sun_time(date, tz=tz, f1=_f1_sunset, f2=_f2_sunset)
+        ss = self._calc_sun_time(date, tz=tz, isRiseTime=False)
         if ss is None:
             raise SunTimeException('The sun never sets on this location (on the specified date)')
         else:
             return ss
 
-    def _calc_sun_time(self, date, tz, f1, f2, zenith=90.8):
+    def _calc_sun_time(self, date, tz, isRiseTime=True, zenith=90.8):
         """
         Calculate sunrise or sunset date.
         :param date: Reference date
@@ -67,13 +67,24 @@ class Sun:
         N = date.timetuple().tm_yday
 
         # 2. convert the longitude to hour value and calculate an approximate time
-        t = f1(N, self.lngHour)
+        if isRiseTime:
+            t = N + ((6 - self.lngHour) / 24)
+        else: #sunset
+            t = N + ((18 - self.lngHour) / 24)
 
-        # 3. get sun true longitude 
-        L = self._get_true_longitude(t)
+        # 3a. calculate the Sun's mean anomaly
+        M = (0.9856 * t) - 3.289
 
-        # 4. get cosH
-        cosH = self._get_cosH(L, zenith=zenith)
+        # 3b. calculate the Sun's true longitude
+        L = M + (1.916 * math.sin(TO_RAD*M)) + (0.020 * math.sin(TO_RAD * 2 * M)) + 282.634
+        L = self._force_range(L, 360) #NOTE: L adjusted into the range [0,360)
+
+        # 4a. calculate the Sun's declination
+        sinDec = 0.39782 * math.sin(TO_RAD*L)
+        cosDec = math.cos(math.asin(sinDec))
+
+        # 4b. calculate the Sun's local hour angle
+        cosH =  (math.cos(TO_RAD*zenith) - (sinDec * math.sin(TO_RAD*self._lat))) / (cosDec * math.cos(TO_RAD*self._lat))
 
         if cosH > 1:
             return None     # The sun never rises on this location (on the specified date)
@@ -81,52 +92,15 @@ class Sun:
             return None     # The sun never sets on this location (on the specified date)
 
         # 4c. finish calculating H and convert into hours
-        H = f2(cosH)
+        if isRiseTime:
+            H = 360 - (1/TO_RAD) * math.acos(cosH)
+        else: #setting
+            H = (1/TO_RAD) * math.acos(cosH)
         H = H / 15
 
-        # 5. get local mean time
-        T = self._get_local_mean_time(t, L, H)
-        
-        # 7a. adjust back to UTC
-        UT = T - self.lngHour
-        
-        if tz:
-            # 7b. adjust back to local time
-            UT += tz.utcoffset(date).total_seconds()/3600
-
-            # 7c. rounding and impose range bounds
-            UT = _force_range(round(UT, 2), 24)
-            time_delta = datetime.timedelta(hours=UT)
-            
-            return datetime.datetime.combine(date, datetime.time(0, 0, tzinfo=tz)) + time_delta
-        else:
-            # 7c. rounding and impose range bounds
-            UT = _force_range(round(UT, 2), 24)
-            time_delta = datetime.timedelta(hours=UT)
-
-            return datetime.datetime.combine(date, datetime.time(0, 0, tzinfo=UTC)) + time_delta
-
-    @staticmethod
-    def _get_true_longitude(t):
-        # 3a. calculate the Sun's mean anomaly
-        M = (0.9856 * t) - 3.289
-
-        # 3b. calculate the Sun's true longitude
-        L = M + (1.916 * math.sin(TO_RAD*M)) + (0.020 * math.sin(TO_RAD * 2 * M)) + 282.634
-        return _force_range(L, 360) #NOTE: L adjusted into the range [0,360)
-        
-    def _get_cosH(self, L, zenith=90.8):
-        # 4a. calculate the Sun's declination
-        sinDec = 0.39782 * math.sin(TO_RAD*L)
-        cosDec = math.cos(math.asin(sinDec))
-
-        # 4b. calculate the Sun's local hour angle
-        return (math.cos(TO_RAD*zenith) - (sinDec * math.sin(TO_RAD*self._lat))) / (cosDec * math.cos(TO_RAD*self._lat))
-
-    def _get_local_mean_time(self, t, L, H):
         # 5a. calculate the Sun's right ascension
         RA = (1/TO_RAD) * math.atan(0.91764 * math.tan(TO_RAD*L))
-        RA = _force_range(RA, 360) #NOTE: RA adjusted into the range [0,360)
+        RA = self._force_range(RA, 360) #NOTE: RA adjusted into the range [0,360)
 
         # 5b. right ascension value needs to be in the same quadrant as L
         Lquadrant  = (math.floor(L/90)) * 90
@@ -138,28 +112,34 @@ class Sun:
 
         # 6. calculate local mean time of rising/setting
         T = H + RA - (0.06571 * t) - 6.622
+        
+        # 7a. adjust back to UTC
+        UT = T - self.lngHour
+        
+        if tz:
+            # 7b. adjust back to local time
+            UT += tz.utcoffset(date).total_seconds()/3600
 
-        return T
+            # 7c. rounding and impose range bounds
+            UT = self._force_range(round(UT, 2), 24)
+            time_delta = datetime.timedelta(hours=UT)
+            
+            return datetime.datetime.combine(date, datetime.time(0, 0, tzinfo=tz)) + time_delta
+        else:
+            # 7c. rounding and impose range bounds
+            UT = self._force_range(round(UT, 2), 24)
+            time_delta = datetime.timedelta(hours=UT)
 
-def _f1_sunrise(N, lngHour):
-    return N + ((6 - lngHour) / 24)
+            return datetime.datetime.combine(date, datetime.time(0, 0, tzinfo=UTC)) + time_delta
 
-def _f1_sunset(N, lngHour):
-    return N + ((18 - lngHour) / 24)
-
-def _f2_sunrise(cosH):
-    return 360 - (1/TO_RAD) * math.acos(cosH)
-
-def _f2_sunset(cosH):
-    return (1/TO_RAD) * math.acos(cosH)
-
-def _force_range(v, max):
-    # force v to be >= 0 and < max
-    if v < 0:
-        return v + max
-    elif v >= max:
-        return v - max
-    return v
+    @staticmethod
+    def _force_range(v, max):
+        # force v to be >= 0 and < max
+        if v < 0:
+            return v + max
+        elif v >= max:
+            return v - max
+        return v
 
 if __name__ == '__main__':
     import datetime
